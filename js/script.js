bdc59db171e40bf2dccfdf7f75fa5d4613d20828
Luiz Henrique <luizhenriquesouza866@icloud.com>
2025-12-01 19:32:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const btnEx01 = document.querySelector("#ex01")
-const btnEx02 = document.querySelector("#ex02")
-const btnEx03 = document.querySelector("#ex03")
-const btnEx04 = document.querySelector("#ex04")
-const btnEx05 = document.querySelector("#ex05")
-const btnEx06 = document.querySelector("#ex06")
-const btnEx07 = document.querySelector("#ex07")
-const btnEx08 = document.querySelector("#ex08")
-const btnEx09 = document.querySelector("#ex09")
+const btnEx01 = document.querySelector("#ex1")
+const btnEx02 = document.querySelector("#ex2")
+const btnEx03 = document.querySelector("#ex3")
+const btnEx04 = document.querySelector("#ex4")
+const btnEx05 = document.querySelector("#ex5")
+const btnEx06 = document.querySelector("#ex6")
+const btnEx07 = document.querySelector("#ex7")
+const btnEx08 = document.querySelector("#ex8")
+const btnEx09 = document.querySelector("#ex9")
 const btnEx10 = document.querySelector("#ex10")
 const btnEx11 = document.querySelector("#ex11")
 const btnEx12 = document.querySelector("#ex12")
@@ -18,32 +18,32 @@ const btnEx17 = document.querySelector("#ex17")
 const btnEx18 = document.querySelector("#ex18")
 const btnEx19 = document.querySelector("#ex19")
 
-function ex01() {
+function ex1() {
     let nome = prompt("Digite seu Nome : ")
     alert("Seja Bem vindo : " + nome)
 }
 
-btnEx01.addEventListener('click', ex01)
+btnEx01.addEventListener('click', ex1)
 
-function ex02() {
+function ex2() {
     let numero = parseInt(prompt("Digite um Número : "))
     let numero02 = parseInt(prompt("Digite outro Número : "))
     let soma = numero + numero02
     alert("O Resultado da soma é = " + soma)
 }
 
-btnEx02.addEventListener('click', ex02)
+btnEx02.addEventListener('click', ex2)
 
-function ex03() {
+function ex3() {
     let numero = parseInt(prompt("Digite um Número : "))
     let numero02 = parseInt(prompt("Digite outro Número : "))
     let subtracao = numero - numero02
     alert("O Resultado da Subtração é = " + subtracao)
 }
 
-btnEx03.addEventListener('click', ex03)
+btnEx03.addEventListener('click', ex3)
 
-function ex04() {
+function ex4() {
     let numero = parseInt(prompt("Digite um Número : "))
     let numero02 = parseInt(prompt("Digite outro Número : "))
     let multiplicacao = numero * numero02
@@ -51,52 +51,52 @@ function ex04() {
 
 }
 
-btnEx04.addEventListener('click', ex04)
+btnEx04.addEventListener('click', ex4)
 
-function ex05() {
+function ex5() {
     let numero = parseFloat(prompt("Digite um Número : "))
     let numero02 = parseFloat(prompt("Digite outro Número : "))
     let divisao = numero / numero02
     alert("O Resultado da Divisão é : " + divisao)
 }
 
-btnEx05.addEventListener('click', ex05)
+btnEx05.addEventListener('click', ex5)
 
-function ex06() {
+function ex6() {
     let numero = parseInt(prompt("Digite um Número : "))
     numero++;
     let numero02 = numero
     alert("Resultado " + numero02)
 }
 
-btnEx06.addEventListener('click', ex06)
+btnEx06.addEventListener('click', ex6)
 
-function ex07() {
+function ex7() {
     let numero = parseInt(prompt("Digite um Número : "))
     numero--;
     let numero02 = numero
     alert("Resultado " + numero02)
 }
 
-btnEx07.addEventListener('click', ex07)
+btnEx07.addEventListener('click', ex7)
 
-function ex08() {
+function ex8() {
     let base = parseInt(prompt("Digite a Base do Retangulo : "))
     let altura = parseInt(prompt("Digite a Altura do Retangulo : "))
     let area = base * altura
     alert("A Area de um Retangulo é : " + area)
 }
 
-btnEx08.addEventListener('click', ex08)
+btnEx08.addEventListener('click', ex8)
 
-function ex09() {
+function ex9() {
     let base = parseInt(prompt("Digite a Base do Triangulo : "))
     let altura = parseInt(prompt("Digite a Altura do Triangulo : "))
     let area = base * altura / 2
     alert("A Area do Triangulo é : " + area)
 }
 
-btnEx09.addEventListener('click', ex09)
+btnEx09.addEventListener('click', ex9)
 
 function ex10() {
     let lado = parseInt(prompt("Digite o Lado do Quadrado : "))
@@ -187,5 +187,6 @@ function ex19() {
     let hipotenusa = (base * base) + (altura * altura)
     alert("O valor da hipotenusa é: " + hipotenusa)
 }
+
 
 btnEx19.addEventListener('click', ex19)
